b65d9248341e157d161c7df087f69faf5d400820
Isaac Marovitz <isaacryu@icloud.com>
2023-04-11 14:40:12 -0400

    Prettier printing and error check

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,13 @@ use crate::coff::{coff_file_header, MachineTypes};
 const IMAGE_DOS_PE_SIGNATURE_OFFSET: usize = 0x3c;
 
 fn main() -> Result<(), Error>{
+    println!("PE Parser - Version 0.1.0");
+    println!("=========================\n");
+
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return Err(Error::new(ErrorKind::InvalidInput, "No file passed to parse!"));
+    }
 
     let file_path = &args[1];
     let binary = fs::read(file_path)
@@ -22,6 +28,8 @@ fn main() -> Result<(), Error>{
 
     if string != "PE\0\0" {
         return Err(Error::new(ErrorKind::InvalidData, "File is not a valid PE!"));
+    } else {
+        println!("Parsing valid PE...");
     }
 
     index += 4;
@@ -29,7 +37,7 @@ fn main() -> Result<(), Error>{
     let header = from_bytes::<coff_file_header>(&binary[index..index+20]);
     let machine_type = MachineTypes::from_u16(header.machine)
         .expect("Failed to get machine type");
-    println!("{:?}", machine_type);
+    println!("Machine Type: {:?}", machine_type);
 
     Ok(())
 }
