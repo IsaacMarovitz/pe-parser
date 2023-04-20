e3449a3e031b65b801f50708fb2eb69dba723ef1
Isaac Marovitz <isaacryu@icloud.com>
2023-04-19 20:15:21 -0400

    Start of tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+// #![warn(missing_docs)]
+// #![deny(missing_doc_code_examples)]
+
 pub mod scribe;
 pub mod coff;
 pub mod optional;
---
+++ tests/pe-parser.rs
@@ -0,0 +1,35 @@
+use std::path::Path;
+use datatest_stable::Result;
+use pe_parser::pe::parse_portable_executable;
+use std::fs;
+
+fn gaunlet(path: &Path) -> Result<()> {
+    let binary = fs::read(path)?;
+    let pe = parse_portable_executable(binary.as_slice())?;
+
+    // Binary passed inital parsing, now check if reserved fields are 0
+
+    if let Some(optional) = pe.optional_header_32 {
+        assert_eq!(optional.data_directories.architecture.size, 0);
+        assert_eq!(optional.data_directories.architecture.virtual_address, 0);
+        assert_eq!(optional.data_directories.reserved.size, 0);
+        assert_eq!(optional.data_directories.reserved.virtual_address, 0);
+
+        assert_eq!(optional.win32_version_value, 0);
+        assert_eq!(optional.loader_flags, 0);
+    }
+
+    if let Some(optional) = pe.optional_header_64 {
+        assert_eq!(optional.data_directories.architecture.size, 0);
+        assert_eq!(optional.data_directories.architecture.virtual_address, 0);
+        assert_eq!(optional.data_directories.reserved.size, 0);
+        assert_eq!(optional.data_directories.reserved.virtual_address, 0);
+
+        assert_eq!(optional.win32_version_value, 0);
+        assert_eq!(optional.loader_flags, 0);
+    }
+
+    Ok(())
+}
+
+datatest_stable::harness!(gaunlet, "tests/pe", r"\.((dat)|(exe)|(dll))$");
